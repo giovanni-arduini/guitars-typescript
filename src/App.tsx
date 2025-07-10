1219700408360622537e4006d738c65e173ceebd
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-10 12:43:48 +0200

    ADD components to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./Contexts/GlobalContext";
 import { FavoritesProvider } from "./Contexts/FavoritesContext";
+import { CompareProvider } from "./Contexts/CompareContext";
+import DefaultLayout from "./Layouts/DefaultLayout";
+import ProductsList from "./Pages/ProductsList";
+import ProductsDetail from "./Pages/ProductDetail";
 
 import "./App.css";
 
@@ -9,11 +13,19 @@ function App() {
     <>
       <GlobalProvider>
         <FavoritesProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route></Route>
-            </Routes>
-          </BrowserRouter>
+          <CompareProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route element={<DefaultLayout />}>
+                  <Route path="/" element={<ProductsList />}></Route>
+                  <Route
+                    path="/guitars/:id"
+                    element={<ProductsDetail />}
+                  ></Route>
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </CompareProvider>
         </FavoritesProvider>
       </GlobalProvider>
     </>
