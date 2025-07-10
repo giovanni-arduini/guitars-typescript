dce3fe21ef6d93f4f561800582a71168e41ce909
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-10 10:02:58 +0200

    ADD CompareContext

---
+++ src/Contexts/CompareContext.tsx
@@ -0,0 +1,92 @@
+import { createContext, useContext, useEffect } from "react";
+import { useGlobalContext } from "./GlobalContext";
+import useCompare from "../Hooks/useCompare";
+import { useFavoritesContext } from "./FavoritesContext";
+import type { Product, Guitar } from "../types";
+
+type CompareContextType = {
+  showCompare: boolean;
+  itemsToCompare: Product[];
+  compareItem: (id: number) => Promise<void>;
+  closeCompare: () => void;
+  toggleCompare: () => void;
+  setShowCompare: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CompareContext = createContext<CompareContextType | null>(null);
+
+export function useCompareContext() {
+  return useContext(CompareContext);
+}
+
+type CompareContextProviderProps = {
+  children: React.ReactNode;
+};
+
+export function CompareProvider({ children }: CompareContextProviderProps) {
+  const globalContext = useGlobalContext();
+  const getProduct = globalContext?.getProduct;
+
+  const favoritesContext = useFavoritesContext();
+  // const setShowFavorites = favoritesContext?.setShowFavorites;
+
+  const { showCompare, setShowCompare, itemsToCompare, setItemsToCompare } =
+    useCompare<Product>();
+
+  async function getItem(id: number): Promise<Guitar> {
+    if (!getProduct) {
+      throw new Error("getProduct is undefined");
+    }
+    const data = await getProduct(id);
+    return data;
+  }
+
+  const compareItem = async (id: number): Promise<void> => {
+    if (!itemsToCompare.some((item) => item.id === id))
+      if (itemsToCompare.length < 3) {
+        const item = await getItem(id);
+        setItemsToCompare((prevItems) => [...prevItems, item]);
+      } else {
+        const item = await getItem(id);
+        setItemsToCompare((prevItems) => [prevItems[1], item]);
+      }
+    else {
+      setItemsToCompare((prevItems) => prevItems.filter((p) => p.id !== id));
+    }
+  };
+
+  // Used in Header, CompareTab
+  function closeCompare() {
+    setShowCompare(false);
+  }
+
+  // Used in Header
+  function toggleCompare() {
+    showCompare ? setShowCompare(false) : setShowCompare(true);
+  }
+
+  useEffect(() => {
+    if (favoritesContext?.setShowFavorites && showCompare) {
+      favoritesContext.setShowFavorites(false);
+    }
+  }, [showCompare]);
+
+  useEffect(() => {
+    itemsToCompare.length < 1 ? setShowCompare(false) : setShowCompare(true);
+  }, [itemsToCompare]);
+
+  return (
+    <CompareContext.Provider
+      value={{
+        showCompare,
+        itemsToCompare,
+        compareItem,
+        closeCompare,
+        toggleCompare,
+        setShowCompare,
+      }}
+    >
+      {children}
+    </CompareContext.Provider>
+  );
+}
